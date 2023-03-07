315766f9d86a9850682cd23a2481f27649a279a4
arifuzzaman35 <arif.zaman@webable.digital>
2023-03-07 12:52:35 +0600

    dashboard modified

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -14,13 +14,7 @@
                         <h5 class="">{{ $inf['title'] }}</h5>
                         <p class="inv-balance">{{ $inf['qty'] }}</p>
                     </div>
-                    <div class="acc-action">
-                        <div class="">
-                            <a href="javascript:void(0);"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg></a>
-                            <a href="javascript:void(0);"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-credit-card"><rect x="1" y="4" width="22" height="16" rx="2" ry="2"></rect><line x1="1" y1="10" x2="23" y2="10"></line></svg></a>
-                        </div>
-                        <a href="javascript:void(0);">Summary</a>
-                    </div>
+                    
                 </div>
             </div>
         </div>
