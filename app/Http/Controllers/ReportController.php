2c704831107a44112426db4f497f61a34c7cc623
arifuzzaman35 <arif.zaman@webable.digital>
2023-09-26 15:08:31 +0600

    solve saleReport time

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -102,13 +102,17 @@ class ReportController extends Controller
             }
             $dataQty = $request->get('per_page') ? $request->get('per_page') : 12;
 
-            $data = Inventory::with('product:id,design_code,category_id,sub_category_id,fragile,fragile_charge','product.category:id,category_name','product.subcategory:id,category_name',
+            $data = Inventory::with('product:id,design_code,category_id,sub_category_id,fragile,fragile_charge',
+                'product.category:id,category_name','product.subcategory:id,category_name',
                 'product.product_brand:id,brand_name','product.product_fabric:id,fabric_name','colour:id,color_name',
                 'product.product_size:id,size_name','product.product_designer:id,designer_name','product.product_embellishment:id,embellishment_name',
                 'product.product_making:id,making_name','product.product_season:id,season_name','product.product_variety:id,variety_name',
                 'product.product_fit:id,fit_name','product.product_artist:id,artist_name','product.product_consignment:id,consignment_name',
                 'product.product_ingredient:id,ingredient_name')
-                ->selectRaw('order_details.product_id, sum(quantity) as sales_quantity,sum(total_selling_price) as total_selling_amount,sum(vat_amount) as total_vat_amount,ROUND(sum(total_buying_price),3) as total_buying_amount,ROUND(sum(total_selling_price - total_buying_price),3) as profit,inventories.stock as current_stock,inventories.sku as p_sku,inventories.colour_id,inventories.size_id,inventories.created_at')
+                ->selectRaw('order_details.product_id, sum(quantity) as sales_quantity,sum(total_selling_price) as total_selling_amount,
+                    sum(vat_amount) as total_vat_amount,ROUND(sum(total_buying_price),3) as total_buying_amount,
+                    ROUND(sum(total_selling_price - total_buying_price),3) as profit,inventories.stock as current_stock,
+                    inventories.sku as p_sku,inventories.colour_id,inventories.size_id')
                 ->leftJoin('order_details', 'inventories.product_id', '=', 'order_details.product_id')
                 ->whereColumn('inventories.product_id', 'order_details.product_id')
                 ->whereColumn('inventories.colour_id', 'order_details.colour_id')
@@ -118,12 +122,13 @@ class ReportController extends Controller
                 ->groupBy('current_stock')
                 ->groupBy('inventories.colour_id')
                 ->groupBy('inventories.size_id')
-                ->groupBy('inventories.created_at')
+                // ->groupBy('inventories.created_at')
                 ->orderByDesc('sales_quantity');
                 if($from != '' && $to != ''){
-                    $data = $data->whereHas('product', function ($q) use ($from,$to) {
-                        $q->whereBetween('created_at', [$from." 00:00:00",$to." 23:59:59"]);
-                    });
+                    $data = $data->whereBetween('order_details.created_at', [$from, $to]);
+                    // $data = $data->whereHas('product', function ($q) use ($from,$to) {
+                    //     $q->whereBetween('created_at', [$from." 00:00:00",$to." 23:59:59"]);
+                    // });
                 }
                 if($category != '' ){
                     $data = $data->whereHas('product', function ($q) use ($category,$subcategory) {
@@ -145,6 +150,7 @@ class ReportController extends Controller
                         $q->where('design_code','like','%'.$keyword.'%');
                     });
                 }
+                //return $data->limit(4)->get();
                 $data = $data->paginate($dataQty);
             return response()->json($data);
         } catch (\Throwable $th) {
